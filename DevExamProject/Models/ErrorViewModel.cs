8883853793d85b44df2f3a6a42188d15ee7262dc
shohag66 <professional.cse66@gmail.com>
2022-09-04 14:18:29 +0600

    Create main mvc project

---
+++ DevExamProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevExamProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
